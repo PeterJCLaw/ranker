444340a8a886fa32ce8712814f5397d18d0b95b6
Peter Law <PeterJCLaw@gmail.com>
2019-12-02 21:04:00 +0000

    Add some classifiers

--- setup.py
+++ setup.py
@@ -21,6 +21,23 @@ setup(
     author='Student Robotics Competition Software SIG',
     author_email='srobo-devel@googlegroups.com',
     license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
     setup_requires=[
         'Sphinx >=1.3, <2'
     ],
