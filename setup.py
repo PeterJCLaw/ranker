812ca1dfe3a6c08eaba6a141ac21424f90b09ad8
Peter Law <PeterJCLaw@gmail.com>
2022-03-12 14:20:45 +0000

    Declare v1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='league-ranker',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=('tests',)),
     package_data={'league_ranker': ['py.typed']},
     url='https://ranker.readthedocs.io/en/latest/',
